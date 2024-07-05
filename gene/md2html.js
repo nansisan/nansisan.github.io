a88c44ea77ef7c2d869d4ad5f1acbc613d859435
nansisan <nansisan@outlook.jp>
2024-07-05 17:33:54 +0900

    Create md2html.js

---
+++ gene/md2html.js
@@ -0,0 +1,77 @@
+function convertMdToHtml(markdown) {
+  let html = '';
+  let inParagraph = false;
+  let inList = false;
+
+  markdown.split('\n').forEach(line => {
+    line = line.trim();
+
+    // ヘッダーの変換 (h1~h6)
+    if (/^#{1,6}\s+.+$/.test(line)) {
+      if (inParagraph) {
+        html += '</p>\n';
+        inParagraph = false;
+      }
+      const headingLevel = line.split(' ')[0].length;
+      const content = line.substring(headingLevel + 1);
+      html += `<h${headingLevel}>${content}</h${headingLevel}>\n`;
+    }
+    // 画像の変換
+    else if (/!\[([^\]]+)\]\(([^\)]+)\)/.test(line)) {
+      if (inParagraph) {
+        html += '</p>\n';
+        inParagraph = false;
+      }
+      html += line.replace(/!\[([^\]]+)\]\(([^\)]+)\)/g, '<img src="$2" alt="$1">\n');
+    }
+    // リンクの変換
+    else if (/\[([^\]]+)\]\(([^\)]+)\)/.test(line)) {
+      if (!inParagraph) {
+        html += '</p>\n';
+        inParagraph = false;
+      }
+      html += line.replace(/\[([^\]]+)\]\(([^\)]+)\)/g, '<a href="$2">$1</a>\n');
+    }
+    // リストの変換
+    else if (line.startsWith('- ')) {
+      if (inParagraph) {
+        html += '</p>\n';
+        inParagraph = false;
+      }
+      if (!inList) {
+        html += '<ul>\n';
+        inList = true;
+      }
+      html += '<li>' + line.substring(2) + '</li>\n';
+    }
+    // 空行の処理
+    else if (line === '') {
+      if (inParagraph) {
+        html += '</p>\n';
+        inParagraph = false;
+      }
+      if (inList) {
+        html += '</ul>\n';
+        inList = false;
+      }
+    }
+    // その他のテキスト（段落）
+    else {
+      if (!inParagraph) {
+        html += '<p>';
+        inParagraph = true;
+      }
+      html += line + ' ';
+    }
+  });
+
+  // 最後の段落またはリストを閉じる
+  if (inParagraph) {
+    html += '</p>\n';
+  }
+  if (inList) {
+    html += '</ul>\n';
+  }
+
+  return html;
+}
